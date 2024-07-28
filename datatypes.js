2148deaaed3a68c684a40a32e2a55c2d08abc76e
Mukesh Batra <R.batra023@gmail.com>
2024-07-28 18:34:30 +0530

    newfile

---
+++ datatypes.js
@@ -0,0 +1,35 @@
+//datatypes:
+//1.integer
+//2.string
+//3.null
+//4.boolean
+
+//integer
+let a = 25;
+console.log(typeof(a),a);
+//string
+let b = "hello";
+console.log(typeof(b),b);
+let email="rbatra@gmail.com"
+console.log(typeof(email),email);
+let para = `knwfnonfo${email}wneofinwoeifnwoienfiwnefoiwnefowinfoienfownefjwcqjwenkmdkwmekdmlkwemkldmlkmwlekmdlkmweldmlwkemdlkwmdlkwmedlkwmeldmwledmlwkemdlkwmedlkwmdlkmwlekdmlwmdjwenwjnwjnwjecnwijc`
+console.log(para)
+//null
+let c = null;
+console.log(typeof(c),c);
+//boolean
+let d = true;
+let z=false;
+console.log(typeof(d),d);
+console.log(typeof(z),z);
+
+//program
+//1.swapping
+ var v=50
+ var n=70
+ console.log("before swapping",v,n)
+ var temp=v;
+ v=n;
+ n=temp;
+ console.log("after swapping",v,n)
+
